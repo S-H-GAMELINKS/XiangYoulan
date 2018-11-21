fccf92ab16689a9186c9df91840ef49174c9696e
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-21 19:59:36 +0900

    fix root path #3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'web/index'
+  root 'web#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
