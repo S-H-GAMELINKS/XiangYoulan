85541c3031118f84c412b417998c1b5ff8ae8310
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-22 23:45:00 +0900

    add post/Edit.vue #4

---
+++ app/javascript/packs/components/post/Edit.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <post-form></post-form>
+    </div>
+</template>
+
+<script>
+import Form from './Form.vue';
+export default {
+    components: {
+        'post-form': Form
+    },
+}
+</script>
