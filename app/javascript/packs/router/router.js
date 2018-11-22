59037ef73297768dbb01329551c2b5eed2dd6b3a
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-23 00:31:05 +0900

    add router in vue.js #4

--- app/javascript/packs/router/router.js
+++ app/javascript/packs/router/router.js
@@ -4,6 +4,10 @@ import VueRouter from 'vue-router';
 import Index from '../components/web/Index.vue';
 import About from '../components/web/About.vue';
 import Contact from '../components/web/Contact.vue';
+import Blogs from '../components/post/Index.vue';
+import Create from '../components/post/Create.vue';
+import Show from '../components/post/Show.vue';
+import Edit from '../components/post/Edit.vue';
 
 Vue.use(VueRouter)
 
@@ -13,5 +17,9 @@ export default new VueRouter({
         { path: '/', component: Index },
         { path: '/about', component: About },
         { path: '/contact', component: Contact },
+        { path: '/blogs', component: Blogs },
+        { path: '/blogs/create', component: Create },
+        { path: '/blogs/:id', component: Show, name: 'blogs'},
+        { path: '/blogs/edit/:id', component: Edit, name: 'edits'},
     ]
 })
