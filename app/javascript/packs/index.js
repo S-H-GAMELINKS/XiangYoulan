272eec52342b8fb17598c1cd94fc78ae94d6b6ad
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-25 12:54:20 +0900

    using vuejs-pagenate in global #59

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue/dist/vue.esm';
 import SocialSharing from 'vue-social-sharing';
+import Paginate from 'vuejs-paginate'
 import Router from './router/router';
 import Header from './components/layouts/Header.vue';
 
 Vue.use(SocialSharing);
+Vue.component('paginate', Paginate)
 
 const app = new Vue({
     el: '#app',
