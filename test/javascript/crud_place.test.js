2fc9d73fe96c88d5d70f6ca4d647437420eb859c
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-25 15:06:59 +0900

    add render show test #55

--- test/javascript/crud_place.test.js
+++ test/javascript/crud_place.test.js
@@ -1,9 +1,15 @@
 import { shallowMount } from "@vue/test-utils";
 import Index from 'conponents/place/Index';
+import Show from 'conponents/place/Show';
 
 describe('CRUD Post', () => {
     it('should render Index', () => {
         const wrapper = shallowMount(Index);
+        expect(wrapper).not.toBeNull();
+    });
+
+    it('should render Show', () => {
+        const wrapper = shallowMount(Show);
         expect(wrapper).not.toBeNull();
     });
 });
