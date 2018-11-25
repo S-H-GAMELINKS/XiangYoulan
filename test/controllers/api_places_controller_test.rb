4f55ceef08e5248178ac10e711c521ed32bdbec8
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-25 16:13:58 +0900

    fix test #55

--- test/controllers/api_places_controller_test.rb
+++ test/controllers/api_places_controller_test.rb
@@ -22,22 +22,22 @@ class ApiPlacesControllerTest < ActionDispatch::IntegrationTest
     end
 
     test "should place create" do
-        post "/api/places", params: {post: {name: "test", content: "test"}}
+        post "/api/places", params: {place: {name: "test", content: "test"}}
         assert_response :success
     end
 
     test "should null place can't create" do
-        post "/api/places", params: {post: {name: "", content: ""}}
+        post "/api/places", params: {place: {name: "", content: ""}}
         assert_equal true, response.body.include?("can't be blank")
     end
 
     test "should place update" do
-        put "/api/places/1", params: {post: {name: "test", content: "test"}}
+        put "/api/places/1", params: {place: {name: "test", content: "test"}}
         assert_response :success
     end
 
     test "should null place can't update" do
-        put "/api/places/1", params: {post: {name: "", content: ""}}
+        put "/api/places/1", params: {place: {name: "", content: ""}}
         assert_equal true, response.body.include?("can't be blank")
     end
 
