eb0e85bc5de8935a77631a821a6be35d7c0e25cf
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-25 15:08:38 +0900

    add render create test #55

--- test/javascript/crud_place.test.js
+++ test/javascript/crud_place.test.js
@@ -1,6 +1,7 @@
 import { shallowMount } from "@vue/test-utils";
 import Index from 'conponents/place/Index';
 import Show from 'conponents/place/Show';
+import Create from 'conponents/place/Create';
 
 describe('CRUD Place', () => {
     it('should render Index', () => {
@@ -10,6 +11,11 @@ describe('CRUD Place', () => {
 
     it('should render Show', () => {
         const wrapper = shallowMount(Show);
+        expect(wrapper).not.toBeNull();
+    });
+
+    it('should render Create', () => {
+        const wrapper = shallowMount(Create);
         expect(wrapper).not.toBeNull();
     });
 });
