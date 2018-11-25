7551903a47738bb706fbd3d1df58334e4bba09f3
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-25 16:05:30 +0900

    add route for Place #55

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,20 @@ Rails.application.routes.draw do
   get "/posts/new", to: "web#index"
   get "/search/:id", to: "web#index"
 
+  get "/places", to: "web#index"
+  get "/places/:id", to: "web#index"
+  get "/places/:id/edit", to: "web#index"
+  get "/places/new", to: "web#index"
+
   devise_for :users
   namespace :api, format: 'json' do
     resources :posts
     post '/posts/search' => 'posts#search'
     post '/posts/pagenation' => 'posts#pagenation'
+
+    resources :places
+    post '/places/search' => 'places#search'
+    post '/places/pagenation' => 'places#pagenation'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
