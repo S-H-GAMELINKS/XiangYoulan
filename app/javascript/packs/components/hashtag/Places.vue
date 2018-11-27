0afb9c44b7ed0855e10092bd252815304f16e212
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-27 22:44:58 +0900

    add hashtag/Places.vue #56

---
+++ app/javascript/packs/components/hashtag/Places.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div class="container">
+            <h1>Places</h1>
+            <p v-for="(place, key, index) in places" :key=index>
+                <router-link :to="{name: 'places_show', params: {id: place.id}}">{{place.name}}</router-link>
+                <router-link :to="{name: 'places_edits', params: {id: place.id}}" >Edit</router-link>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+import $ from 'jquery';
+
+export default {
+    data: function() {
+        return {
+            places: [],
+            tag: String(this.$route.path).replace(/\/hashtags\//, '')
+        }
+    },
+    mounted: function() {
+        this.getPlaces();
+    },
+    methods: {
+        getPlaces: function() {
+            axios.defaults.headers['X-CSRF-TOKEN'] = $('meta[name=csrf-token]').attr('content');
+            axios.defaults.headers['content-type'] = 'application/json';
+
+            this.places.length = 0;
+
+            axios.post('/api/places/hashtags', {tag: this.tag}).then((response) => {
+                for(var i = 0; i < response.data.length; i++) {
+                    this.places.push(response.data[i]);
+                }
+            }, (error) => {
+                console.log(error);
+            })
+        },
+    }
+
+}
+</script>
