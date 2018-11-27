9bd85e543f5a837f7f4df9857450b8bac87040c5
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-27 20:50:12 +0900

    DB migrate #56

--- db/schema.rb
+++ db/schema.rb
@@ -26,8 +26,6 @@ ActiveRecord::Schema.define(version: 2018_11_26_083232) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "latitude"
-    t.float "longitude"
   end
 
   create_table "taggings", force: :cascade do |t|
