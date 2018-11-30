561b439bf909eb443c003b506ddaa6ce01421f30
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-30 23:50:11 +0900

    add test should render User Edit #53

--- test/javascript/users_test.js
+++ test/javascript/users_test.js
@@ -1,9 +1,15 @@
 import { shallowMount } from "@vue/test-utils";
-import MyPages from 'components/users/MyPages';
+import MyPages from 'components/user/MyPages';
+import Edit from 'components/user/Edit';
 
 describe('Users pages', () => {
     it('should render User mypages', () => {
         const wrapper = shallowMount(Index);
+        expect(wrapper).not.toBeNull();
+    });
+
+    it('should render User Edit', () => {
+        const wrapper = shallowMount(Edit);
         expect(wrapper).not.toBeNull();
     });
 });
