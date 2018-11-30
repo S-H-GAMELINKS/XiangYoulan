23f444e1329ebf5d1d432315ecace208d5188708
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-30 23:46:42 +0900

    add test "should update user info" #53

--- test/controllers/devise_controller_test.rb
+++ test/controllers/devise_controller_test.rb
@@ -18,4 +18,11 @@ class DeviseControllerTest < ActionDispatch::IntegrationTest
     delete "/users/sign_out"
     assert_response :redirect
   end
+
+  test "should update user info" do
+    post "/users", params: { user: { email: "test@hoge.com", password: "testhogehoge", password_confirmation: "testhogehoge" }}
+    post "/users/sign_in", params: { user: { email: "test@hoge.com", password: "testhogehoge" }}
+    put "/api/users", params: { user: { email: "test@hogehoge.com", password: "testhogehoge", password_confirmation: "testhogehoge" }}
+    assert_equal true,  User.first.email == "test@hogehoge.com"
+  end
 end
