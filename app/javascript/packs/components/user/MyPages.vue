0f374d21d9bc89267055211f54c74197e97a2d52
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-02 18:47:26 +0900

    add user/MyPages.vue #53

---
+++ app/javascript/packs/components/user/MyPages.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <label> Name: {{name}} </label>
+        <label> Email: {{email}} </label>
+        <label> About: </label>
+        <span v-html="about"> </span>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            name: this.$store.state.user.name,
+            email: this.$store.state.user.email,
+            about: this.$store.state.user.about
+        }
+    }
+}
+</script>
