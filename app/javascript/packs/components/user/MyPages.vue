3a02da40138cce0b3feb9abc1632e79ef490ad0b
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-03 17:47:07 +0900

    fix mypages layout #53

--- app/javascript/packs/components/user/MyPages.vue
+++ app/javascript/packs/components/user/MyPages.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="container">
-        <router-link to="/users/mypages/edit">Edit</router-link>
-        <label> Name: {{$store.state.user.name}} </label>
-        <label> Email: {{$store.state.user.email}} </label>
-        <label> About: </label>
+        <p><label> Name: {{$store.state.user.name}} </label></p>
+        <p><label> Email: {{$store.state.user.email}} </label></p>
+        <p><label> About: </label></p>
         <span v-html="$store.state.user.about"> </span>
+
+        <router-link to="/users/mypages/edit" class="button">
+            <button type="button" class="btn btn-primary">Edit</button>
+        </router-link>
     </div>
 </template>
