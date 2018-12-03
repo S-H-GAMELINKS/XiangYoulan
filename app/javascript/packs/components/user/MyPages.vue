7f05a8e4ef3fa46a6c111532c1ee430553ea1af2
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-03 15:55:51 +0900

    add edit link in mypages #53

--- app/javascript/packs/components/user/MyPages.vue
+++ app/javascript/packs/components/user/MyPages.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <router-link to="/users/mypages/edit">Edit</router-link>
         <label> Name: {{$store.state.user.name}} </label>
         <label> Email: {{$store.state.user.email}} </label>
         <label> About: </label>
