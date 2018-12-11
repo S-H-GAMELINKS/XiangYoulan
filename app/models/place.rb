9c87ea9b10b15214fc834d144231f361b1618a13
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-11 19:30:24 +0900

    add follow relationship #24

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,5 @@ class Place < ApplicationRecord
     validates :name, presence: true
     validates :content, presence: true
     acts_as_taggable
+    acts_as_followable
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
     acts_as_taggable
+    acts_as_follower
 end
