2198347e4d580e5003734c6b258e8a754ede8722
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-15 19:45:27 +0900

    add test 'should render Web/NowPlace' #64

--- test/javascript/web.test.js
+++ test/javascript/web.test.js
@@ -2,6 +2,7 @@ import { shallowMount } from "@vue/test-utils";
 import Index from 'components/web/Index';
 import About from 'components/web/About';
 import Contact from 'components/web/Contact';
+import NowPlace from 'components/web/NowPlace';
 
 describe('Test Web', () => {
     it('should render Web/Index', () => {
@@ -16,6 +17,11 @@ describe('Test Web', () => {
 
     it('should render Web/Contact', () => {
         const wrapper = shallowMount(Contact);
+        expect(wrapper).not.toBeNull();
+    });
+
+    it('should render Web/NowPlace', () => {
+        const wrapper = shallowMount(NowPlace);
         expect(wrapper).not.toBeNull();
     });
 })
