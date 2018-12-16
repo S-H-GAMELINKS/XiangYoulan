36debead4f6886fd4600a8db2e1027e03600729b
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-16 12:31:03 +0900

    tracked activity in place model #25

--- app/models/place.rb
+++ app/models/place.rb
@@ -5,4 +5,6 @@ class Place < ApplicationRecord
     acts_as_taggable
     acts_as_followable
     acts_as_mappable :lat_column_name => :latitude, :lng_column_name => :longitude
+    include PublicActivity::Model
+    tracked
 end
