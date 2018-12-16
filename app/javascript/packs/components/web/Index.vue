85d410c4aa873998fa83d7092190ac015e511664
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-16 16:29:16 +0900

    add content in Web/Index #97

--- app/javascript/packs/components/web/Index.vue
+++ app/javascript/packs/components/web/Index.vue
@@ -1,5 +1,11 @@
 <template>
     <div class="container">
-        Index Pages
+        <p><h1>Index Pages</h1></p>
+        <p>
+            Xiang Youlan へようこそ！<br><br>
+            Xiang Youlan はユーザーの皆さんが訪れた観光地の情報を共有できるWebサービスです！<br><br>
+            <router-link class="btn btn-primary" to="/sign_up">アカウントを作る</router-link><br><br><br>
+            <router-link class="btn btn-success" to="/sign_in">ログイン</router-link>
+        </p>
     </div>
 </template>
