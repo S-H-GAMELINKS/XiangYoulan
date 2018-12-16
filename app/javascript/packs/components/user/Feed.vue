fc2d85640052ca219119733b5b6e58f9c65fba19
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-16 15:41:11 +0900

    add follow place feed  #25

--- app/javascript/packs/components/user/Feed.vue
+++ app/javascript/packs/components/user/Feed.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="container">
-
+        <span v-for="(feed, key, index) in feeds" :key=index>
+            <span v-for="(place, key, index) in places" :key=index>
+                <div v-if="place.id === feed.owner_id">
+                    <div class="container">
+                        <router-link :to="{name: 'places_show', params: {id: place.id}}">{{place.name}}</router-link>
+                    </div>
+                    <div class="container">
+                        <router-link :to="{name: 'posts_show', params: {id: place.id, post_id: feed.trackable_id}}">フォローしている場所に新しい投稿がされました</router-link>
+                    </div>
+                    <br>
+                </div>
+            </span>
+        </span>
     </div>
 </template>
 
@@ -41,8 +53,12 @@ export default {
             axios.defaults.headers['X-CSRF-TOKEN'] = $('meta[name=csrf-token]').attr('content');
             axios.defaults.headers['content-type'] = 'application/json';
 
+            this.getAllPlaces();
+
             axios.get('/api/users/feed').then((response) => {
-                console.log(response);
+                for(var i = 0; i < response.data.length; i++) {
+                    this.feeds.push(response.data[i]);
+                }
             }, (error) => {
                 console.log(error);
             })
