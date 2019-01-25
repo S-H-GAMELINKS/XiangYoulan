0a2aa8cf4d96c0e39ba7f5e89ef5dbacd006f5a1
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-25 10:33:22 +0900

    fix api::place::follow #121

--- app/controllers/api/places_controller.rb
+++ app/controllers/api/places_controller.rb
@@ -57,7 +57,7 @@ class Api::PlacesController < ApplicationController
     # POST /api/places/follow
     def follow
       if current_user.follows?(@place)
-        render json: true
+        render json: false
       else
         render json: current_user.follow!(@place)
       end
